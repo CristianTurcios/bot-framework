8e7027468c386a64e9793fd3dad88a6c407e35b0
cesar-barahona <cesar.barahona@laureate.net>
2020-05-18 17:35:13 -0600

    axios

--- src/bot.ts
+++ src/bot.ts
@@ -2,6 +2,7 @@
 // Licensed under the MIT License.
 
 import { ActivityHandler, MessageFactory } from 'botbuilder';
+const axios = require('axios');
 
 export class EchoBot extends ActivityHandler {
     constructor() {
@@ -9,6 +10,7 @@ export class EchoBot extends ActivityHandler {
         // See https://aka.ms/about-bot-activity-message to learn more about the message and other activity types.
         this.onMessage(async (context, next) => {
             const replyText = `Echo: ${ context.activity.text }`;
+            this.getAvaamoResponse(context.activity.text);
             await context.sendActivity(MessageFactory.text(replyText, replyText));
             // By calling next() you ensure that the next BotHandler is run.
             await next();
@@ -26,4 +28,29 @@ export class EchoBot extends ActivityHandler {
             await next();
         });
     }
+
+getAvaamoResponse(userText:String){
+
+    axios.post('/https://c0.avaamo.com//bot_connector_webhooks/be09efe0-1612-4419-8f26-34b79c072cd8/message.json', {
+        channel_uuid: '1c954349-df14-4665-9311-92ea548242f2',
+        user: {
+          first_name : 'Sam',
+          last_name : 'Will',
+          uuid: '9ac15843-151d-47fb-8b3d-930b89ce797e'
+        },
+        message: {
+          text: userText
+        },
+        
+      })
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
 }
+
+
+}
+
