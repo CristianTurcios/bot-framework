e8294c536ba4f9a9f396930f19cfacd49826e3c6
Cristian Turcios <cristian.turcios@laureate.net>
2020-05-18 18:00:35 -0600

    get response from avaamo

--- src/bot.ts
+++ src/bot.ts
@@ -9,9 +9,13 @@ export class EchoBot extends ActivityHandler {
         super();
         // See https://aka.ms/about-bot-activity-message to learn more about the message and other activity types.
         this.onMessage(async (context, next) => {
-            const replyText = `Echo: ${ context.activity.text }`;
-            this.getAvaamoResponse(context.activity.text);
-            await context.sendActivity(MessageFactory.text(replyText, replyText));
+            const replyText = `Echo: ${context.activity.text}`;
+            const avaamoResponse = await this.getAvaamoResponse(context.activity.text);
+            try {
+                await context.sendActivity(MessageFactory.text(avaamoResponse, avaamoResponse));
+            } catch (error) {
+                await context.sendActivity(MessageFactory.text(replyText, replyText));
+            }
             // By calling next() you ensure that the next BotHandler is run.
             await next();
         });
@@ -29,28 +33,25 @@ export class EchoBot extends ActivityHandler {
         });
     }
 
-getAvaamoResponse(userText:String){
+    public async getAvaamoResponse(userText: string) {
+        try {
+            const response = await axios.post('https://c0.avaamo.com//bot_connector_webhooks/be09efe0-1612-4419-8f26-34b79c072cd8/message.json', {
+                channel_uuid: '1c954349-df14-4665-9311-92ea548242f2',
+                message: {
+                    text: userText
+                },
+                user: {
+                    first_name: 'Sam',
+                    last_name: 'Will',
+                    uuid: '9ac15843-151d-47fb-8b3d-930b89ce797e'
+                }
+            });
 
-    axios.post('/https://c0.avaamo.com//bot_connector_webhooks/be09efe0-1612-4419-8f26-34b79c072cd8/message.json', {
-        channel_uuid: '1c954349-df14-4665-9311-92ea548242f2',
-        user: {
-          first_name : 'Sam',
-          last_name : 'Will',
-          uuid: '9ac15843-151d-47fb-8b3d-930b89ce797e'
-        },
-        message: {
-          text: userText
-        },
-        
-      })
-      .then(function (response) {
-        console.log(response);
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
+            console.log(response.data.incoming_message.bot_replies);
+            return response.data.incoming_message.bot_replies[0].text;
+
+        } catch (e) {
+            console.log(e);
+        }
+    }
 }
-
-
-}
-
